3d98c7e6905a06316cb51729b52d6a7597360133
Carolyn <carolynwk@gmail.com>
2019-03-10 18:10:15 -0700

    Almost finished with script; added css styling.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,60 +2,69 @@
 
 var wins = 0;
 var losses = 0;
-var numGuesses = 8;
-var letters;
-var lettersGuessed = [];
+var numGuesses;
+var lettersGuessed;
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var letter;
+var compLetter;
 
-function randLetter() {
 
+function newGame() {
+    
+    lettersGuessed = [];
+    numGuesses = 3;
     document.getElementById('guessesLeft').textContent = numGuesses;
     document.getElementById('wins').textContent = wins;
     document.getElementById('losses').textContent = losses;
-
+    
     //Computer selects a random letter
+    var compLetter = letters[Math.floor(Math.random() * letters.length)];
+    guessGame();
+    console.log(compLetter);
+    return compLetter;
+}
 
 
-    var letter = letters[Math.floor(Math.random() * letters.length)];
-
-    console.log(letter);
-
-    //While numGuesses is > 0:
+function guessGame() {
 
     document.onkeyup = function (x) {
         var x = event.key;
         //On key event, compare key pressed var letters to validate a letter was selected
         //if not a letter, 
         if (!letters.includes(x)) {
-            alert("That/'s not a letter. Please press a letter key.");
+            alert("That's not a letter. Please press a letter key.");
 
-            //Compare user guess to computer letter
-            //if not a match, alert ("Sorry, try again!");
-            // losses++;
-            // numGuesses--;
-        } else if (x !== letter) {
-            alert("Sorry, guess again.");
-            numGuesses--;
-            document.getElementById('guessesLeft').textContent = numGuesses;
-            //append x to lettersGuessed[]
-        }
-        //if match, alert ('You Win!  Let's play again.')
-        // wins++;
+        //Compare user guess to computer letter
+        //if not a match, alert ("Sorry, try again!");
+        // losses++;
+        // numGuesses--;
+        } 
         else {
-            alert("You win! Let's play again.");
-            wins++;
-            document.getElementById('wins').textContent = wins;
-            numGuesses = 8;
-            randLetter();
+            if (x !== compLetter) {
+                if (numGuesses > 1) {
+
+                    alert("Sorry, guess again.");
+                    numGuesses--;
+                    document.getElementById('guessesLeft').textContent = numGuesses;
+                    //append x to lettersGuessed[]
+                    lettersGuessed.push(x)
+                    document.getElementById('guesses').innerHTML = lettersGuessed
+                } else {
+                    alert("Sorry, you lose!  The answer was " + compLetter + ".  Let's play again.");
+                    losses++;
+                    document.getElementById('guesses').innerHTML = ''
+
+                    newGame();
+                }
+            }
+            //if match, alert ('You Win!  Let's play again.')
+            else {
+                alert("You win! Let's play again.");
+                wins++;
+                document.getElementById('wins').textContent = wins;
+                newGame();
+            }
         }
+
     }
-
-    //When numGuesses = 0 :
-    // alert("Sorry, you lose!  Let's play again.");
-    // losses--;
-    // randLetter();
-
-
 
 };
